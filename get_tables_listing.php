f63076ffdab777710ef2703b11a7fab02700bf4b
yury-n <yury.nedomolkin@gmail.com>
2016-08-29 01:25:33 -0700

    Be sure to create an empty array for indexed/nonindexed columns even if theres none of such type

--- get_tables_listing.php
+++ get_tables_listing.php
@@ -13,7 +13,16 @@ foreach ($rows as $row) {
     $column = $row['COLUMN_NAME'];
     $is_indexed = ($row['COLUMN_KEY'] != '');
 
-    $results[$table][$is_indexed ? 'indexed_columns' : 'nonindexed_columns'][] = $column;
+    if (!isset($results[$table]['indexed_columns'])) {
+        $results[$table]['indexed_columns'] = [];
+    }
+    if (!isset($results[$table]['nonindexed_columns'])) {
+        $results[$table]['nonindexed_columns'] = [];
+    }
+
+    $column_group = ($is_indexed ? 'indexed_columns' : 'nonindexed_columns');
+
+    $results[$table][$column_group][] = $column;
 }
 
 echo json_encode($results);
